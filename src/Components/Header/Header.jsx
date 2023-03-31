26057b15522bdcb0be490d1145033053d94f1c8d
asifikbal09 <asif.ikbal662@gmail.com>
2023-04-01 00:35:40 +0600

    header Added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Knowledge Cafe</h1>
+            <div className='block'></div>
+
+        </div>
+    );
+};
+
+export default Header;
